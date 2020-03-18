0f5e7d4ef680d637729f5e294fa0a7b0801e024a
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2020-03-18 20:46:17 -0300

    Add validation

--- aula-7/js/index.js
+++ aula-7/js/index.js
@@ -22,20 +22,47 @@ function sumValues() {
     let inputB = document.getElementById('secondNumber');
     let inputC = document.getElementById('result');
 
-    if (isInputValueValid(inputA) && isInputValueValid(inputB)) {
-        let a = parseInt(inputA.value, 10);
-        let b = parseInt(inputB.value, 10);
-        
-        let resultado = a + b;
-        inputC.value = resultado;
-    } else {
-        alert('Por favor, preencha todos os campos!');
+    if (isInputValueValid(inputA, 'firstNumberError') && isInputValueValid(inputB, 'secondNumberError')) {
+        let a = convertToDecimal(inputA, 'firstNumberError');
+        let b = convertToDecimal(inputB, 'secondNumberError');
+
+        if (!isNaN(a) && !isNaN(b)) {
+            let resultado = a + b;
+            inputC.value = resultado;
+        }
     }
 }
 
-function isInputValueValid(input) {
-    if (input.value === '') {
+function isInputValueValid(input, idErrorMessage) {
+    if (input.value.trim() === '') {
+        input.focus();
+        addRemoveHideMessageClass(idErrorMessage, false);
         return false;
+    } else {
+        addRemoveHideMessageClass(idErrorMessage, true);
     }
     return true;
+}
+
+function convertToDecimal(input, idErrorMessage) {
+    let temp = parseInt(input.value, 10);
+    if (isNaN(temp)) {
+        input.focus();
+        addRemoveHideMessageClass(idErrorMessage, false);
+    } else {
+        addRemoveHideMessageClass(idErrorMessage, true);
+    }
+
+    return temp;
+}
+
+function addRemoveHideMessageClass(idElement, addClass) {
+    let element = document.getElementById(idElement);
+    if (addClass) {
+        if (element.className.indexOf('hideMessage') === -1) {
+            element.className = element.className + ' hideMessage'; 
+        }
+    } else {
+        element.className = element.className.replace('hideMessage', '').trim();
+    }
 }
