6f3b7478c2ad3829285239ba5413643f543737e2
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2020-03-27 08:34:11 -0300

    Add aula-9

--- aula-9/js/index.js
+++ aula-9/js/index.js
@@ -5,11 +5,15 @@ function start() {
     button.onclick = function() {
         addNewParagraph();
     };
+
+    let addButton = document.getElementById('add');
+    addButton.onclick = addNewListItem;
 }
 
 function addNewParagraph() {
-    console.log('ALEXANDRE!');
     let newP = document.createElement('p');
+
+    newP.onclick = removeParagraph;
 
     let div = document.getElementById('container');
 
@@ -20,4 +24,45 @@ function addNewParagraph() {
     let newTextNode = document.createTextNode(text);
 
     newP.appendChild(newTextNode);
+}
+
+function removeParagraph() {
+    let myParent = this.parentNode;
+    myParent.removeChild(this);
+}
+
+function addNewListItem() {
+    let myList = document.getElementById('lista');
+    let inputName = document.getElementById('name').value.trim();
+
+    if (inputName === '') {
+        alert('Por favor digite algo válido!');
+    
+    } else if (alreadyExist(myList, inputName)) {
+        alert('Esse nome já está na lista! Digite outro, por favor!');
+
+    } else {
+        let li = createNewListItem(inputName);
+        myList.appendChild(li);
+    }
+}
+
+function alreadyExist(myList, newName) {
+    let child = myList.children;
+
+    for (let i = 0; i < child.length; i++) {
+        let li = child[i];
+        if (li.innerHTML === newName) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function createNewListItem(value) {
+    let newLi = document.createElement('li');
+    let text = document.createTextNode(value);
+    newLi.appendChild(text);
+    return newLi;
 }
