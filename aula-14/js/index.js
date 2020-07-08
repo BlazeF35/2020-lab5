88daa4845c60ca3ff6c4b2ff00508aea9f4aad21
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2020-07-08 20:59:30 -0300

    Create a course

--- aula-14/js/index.js
+++ aula-14/js/index.js
@@ -1,6 +1,14 @@
 start();
 
 function start() {
+    loadDataFromServer();
+    let saveButton = document.getElementById('addCourseBtn');
+    saveButton.onclick = function() {
+       save();
+    };
+}
+
+function loadDataFromServer() {
     $.get('https://pacific-wave-50441.herokuapp.com/api/courses', function(data) {
         addDataInTable(data);
     });
@@ -9,6 +17,7 @@ function start() {
 function addDataInTable(data) {
     let table = document.getElementById('cursos');
     let tbody = table.tBodies[0];
+    tbody.innerHTML = '';
 
     data.forEach(function(item) {
         let line = document.createElement('tr');
@@ -22,5 +31,16 @@ function addDataInTable(data) {
         line.appendChild(workloadColumn);
 
         tbody.appendChild(line);
+    });
+}
+
+function save() {
+    let newCourse = {
+        name: document.getElementById('name').value,
+        workload: document.getElementById('workload').value
+    };
+    
+    $.post("https://pacific-wave-50441.herokuapp.com/api/courses", newCourse).done(function() {
+        loadDataFromServer();
     });
 }
